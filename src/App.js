403beb979e82b1b9ddef60078e98fa72d5dcbc2b
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-05-15 19:46:56 +0500

    added css to expenseitem.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import ExpenseItem from './components/ExpenseItem';
-import logo from './logo.svg';
+import ExpenseItem from "./components/ExpenseItem";
+import logo from "./logo.svg";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Tissue Paper",
+      amount: 28.98,
+      date: new Date(2020, 10, 20),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 9, 5),
+    },
+    {
+      id: "e3",
+      title: "Bell",
+      amount: 100,
+      date: new Date(2022, 3, 11, 20, 55, 33, 999),
+    },
+    {
+      id: "e4",
+      title: "New Desk (wooden)",
+      amount: 450,
+      date: new Date(2022, 1, 9),
+    },
+  ];
+
+  const expenseItems = expenses.map((item) => (
+    <ExpenseItem 
+        title={item.title} 
+        amount={item.amount}
+        date={item.date}
+    />
+  ));
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenseItems}
     </div>
   );
 }
