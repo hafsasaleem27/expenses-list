812e45ac858703a8f69634e3659b3fd7ab891e88
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-05-16 23:27:48 +0500

    added new component for expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -13,26 +14,13 @@ const Expenses = (props) => {
         setSelectedYear(selectedYear);
     };
 
-    let expensesContent = <p>No expenses found!</p>;
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map(item => 
-            <ExpenseItem
-                key={item.id}
-                title={item.title}
-                amount={item.amount}
-                date={item.date}
-            />
-        );
-    }
-
     return (
         <Card className="expenses">
             <ExpensesFilter 
                 selected={filteredYear}
                 onChangeFilter={filterChangeHandler}
             />
-            {expensesContent}
+            <ExpensesList items={filteredExpenses} />
         </Card>
     );
 };
